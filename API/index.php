052ce63b088e814c6d5360f9ceed4c3dbc9bc0ff
Pindagus <agusnobel@gmail.com>
2014-10-30 22:47:56 +0100

    Added API route

--- API/index.php
+++ API/index.php
@@ -1,6 +1,37 @@
 <?php
 require_once "global.php";
 
+$data = null;
+$token = $_GET["Token"]; //TODO: Implement tokens/security
+$id = $_GET["Id"];
+
+if(empty($id) || $id == "" || !is_numeric($id))
+{
+    echo "Invalid id";
+    exit();
+}
+
+switch($_GET["Object"])
+{
+    case "User":
+        $data = new User($Database, $id);
+        break;
+    case "Field":
+        $data = new Field($Database, $id);
+        break;
+    case "Seed":
+        $data = new Seed($Database, $id);
+        break;
+    case "Product":
+        $data = new Product($Database, $id);
+        break;
+}
+
 header('Content-type: application/json');
 
-echo json_encode(new User($Database, 1));
+echo json_encode(
+    array(
+        "token" => $token,
+        "data" => $data
+    )
+);
